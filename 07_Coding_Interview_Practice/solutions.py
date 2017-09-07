68080f1a7bab470fc50c8cb6b026a1f3476dcc0b
Charlio Xu <chalioxumykj@gmail.com>
2017-09-07 16:55:34 -0700

    feat: set up solutions.py and explanations.txt files in 07 interview practice

---
+++ 07_Coding_Interview_Practice/solutions.py
@@ -0,0 +1,30 @@
+"""
+solutions to the technical interview practice of Machine Learning
+Engineer Nanodegree at Udacity
+"""
+
+def question1():
+    #TODO
+    pass
+    
+def question2():
+    #TODO 
+    pass
+    
+def question3():
+    #TODO
+    pass
+    
+def question4():
+    #TODO
+    pass
+   
+def question5():
+    #TODO
+    pass
+    
+
+"""
+three test cases for each questions
+"""
+   
