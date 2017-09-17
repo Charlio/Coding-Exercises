22353632367f5f0c49035e9997f00a718c215749
Charlio Xu <chalioxumykj@gmail.com>
2017-09-16 19:36:30 -0700

    01_TwoSum

---
+++ 00_Coding_Problems/01_TwoSum.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d = {}
+        for i,n in enumerate(nums):
+            if n in d:
+                return d[n], i
+            d[target-n] = i
+        
