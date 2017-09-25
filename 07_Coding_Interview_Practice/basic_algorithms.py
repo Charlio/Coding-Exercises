97a6885e5f8e3fac27d0cb9b3f408e75bf7d7837
Charlio Xu <chalioxumykj@gmail.com>
2017-09-25 14:48:33 -0700

    feat: basic algorithms

---
+++ 07_Coding_Interview_Practice/basic_algorithms.py
@@ -0,0 +1,36 @@
+'''
+Basic algortihms
+'''
+
+'''
+binary search
+'''
+
+
+
+'''
+Tree: binary search tree traversal: pre-, in-, post- order
+Heaps: popout, insert
+'''
+
+'''
+Map, Hashing
+'''
+
+
+'''
+Sort: mergesort, quicksort, bubblesort
+'''
+
+'''
+graph: depth-first, breadth-first search, shortest path(Dijkstra), minimum spanning tree 
+'''
+
+'''
+dynamic programming: stock transactions
+'''
+
+'''
+knapsack, traveling salesman
+'''
+
