bb6dd257ceb8ed3d16884e7295114d8dfd84a733
Charlio Xu <chalioxumykj@gmail.com>
2017-10-14 18:39:08 -0700

    95_ContainsDuplicateII

---
+++ 00_Coding_Problems/95_ContainsDuplicateII.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> num_map;
+        for(int i = 0; i < nums.size(); i++) {
+            if(num_map.find(nums[i]) != num_map.end() && i-num_map[nums[i]] <= k) {
+                return true;
+            } else {                    
+                num_map[nums[i]] = i;
+            }
+        }
+        return false;
+    }
+};
