be3628fd5911bcb9fa7fa2ca2484e2ab22d59e1f
Charlio Xu <chalioxumykj@gmail.com>
2017-10-20 17:25:44 -0700

    016_MaxHourglassSum2DArray.cpp

---
+++ 000_Hackerrank/016_MaxHourglassSum2DArray.cpp
@@ -0,0 +1,45 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    vector< vector<int> > arr(6,vector<int>(6));
+    for(int arr_i = 0;arr_i < 6;arr_i++){
+       for(int arr_j = 0;arr_j < 6;arr_j++){
+          cin >> arr[arr_i][arr_j];
+       }
+    }
+    
+    int hourglass_max = INT8_MIN;
+    for(int i = 1; i < 5; i++) {
+        for(int j = 1; j < 5; j++) {
+            int hourglass_val = arr[i-1][j-1] + arr[i-1][j] + arr[i-1][j+1] + arr[i][j] + arr[i+1][j-1] + arr[i+1][j] + arr[i+1][j+1];
+            hourglass_max = max(hourglass_max, hourglass_val);
+        }
+    }
+    cout << hourglass_max << endl;
+    
+    return 0;
+}
